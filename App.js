a6c074f64f836ccb1dd7918369d8207fd2bc4343
sanricl <sandersonricl@gmail.com>
2022-07-17 15:11:21 -0300

    Theme Provider setup

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import RestaurantScreen from './src/features/restaurants/screens/RestaurantScreen';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { ThemeProvider } from 'styled-components/native';
 
+import { theme } from './src/infrastructure/theme';
 
 export default function App() {
   return (
     <>
-      <RestaurantScreen />
-      <ExpoStatusBar style="auto" />
+      <ThemeProvider theme={theme}>
+        <RestaurantScreen />
+        <ExpoStatusBar style="auto" />
+      </ThemeProvider>
     </>
   );
 }
