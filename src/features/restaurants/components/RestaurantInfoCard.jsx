be7cb83ab725af9f686fba70478583fd55f4474e
sanricl <sandersonricl@gmail.com>
2022-07-18 11:34:12 -0300

    Fonts cleanup

--- src/features/restaurants/components/RestaurantInfoCard.jsx
+++ src/features/restaurants/components/RestaurantInfoCard.jsx
@@ -2,9 +2,15 @@ import { Card } from 'react-native-paper';
 import styled from 'styled-components/native';
 
 const Title = styled.Text`
-  padding: ${(props) => props.theme.space[3]};
+  color: ${(props) => props.theme.colors.text.primary};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+`;
+
+const Address = styled.Text`
   color: ${(props) => props.theme.colors.text.primary};
   font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
 `;
 
 const RestaurantCard = styled(Card)`
@@ -16,6 +22,10 @@ const RestaurantCardCover = styled(Card.Cover)`
   padding: ${(props) => props.theme.space[3]};
 `;
 
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
 const RestaurantInfoCard = ({ data }) => {
   return (
     <RestaurantCard elevation={5}>
@@ -24,7 +34,10 @@ const RestaurantInfoCard = ({ data }) => {
           uri: 'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg',
         }}
       />
-      <Title>{data.name}</Title>
+      <Info>
+        <Title>{data.name}</Title>
+        <Address>{data.address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
