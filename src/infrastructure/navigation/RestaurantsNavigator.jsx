956ff6be691f2200eb6fcb8435586ef22af57bdb
sanricl <sandersonricl@gmail.com>
2022-07-29 13:29:28 -0300

    Stored Favourites

--- src/infrastructure/navigation/RestaurantsNavigator.jsx
+++ src/infrastructure/navigation/RestaurantsNavigator.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
 import RestaurantScreen from '../../features/restaurants/screens/RestaurantScreen';
-import { Text } from 'react-native';
 import RestaurantDetailScreen from '../../features/restaurants/screens/RestaurantDetailScreen';
 
 const RestaurantStack = createStackNavigator();
@@ -9,7 +8,7 @@ const RestaurantStack = createStackNavigator();
 const RestaurantsNavigator = () => {
   return (
     <RestaurantStack.Navigator screenOptions={{ headerShown: false, ...TransitionPresets.ModalPresentationIOS, }}>
-      <RestaurantStack.Screen name="Restaurants" component={RestaurantScreen} />
+      <RestaurantStack.Screen name="Restaurant" component={RestaurantScreen} />
       <RestaurantStack.Screen name="RestaurantDetail" component={RestaurantDetailScreen} />
     </RestaurantStack.Navigator>
   );
