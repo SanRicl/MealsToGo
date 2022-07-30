f8240afc1b7bdd53382fa0be2ceb647f4a3558b6
sanricl <sandersonricl@gmail.com>
2022-07-30 16:45:16 -0300

    Firebase config setup

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components/native';
 import { theme } from './src/infrastructure/theme';
@@ -9,14 +9,39 @@ import { LocationContextProvider } from './src/services/location/locationContext
 import { FavouritesContextProvider } from './src/services/favourites/favouritesContext';
 
 import { initializeApp } from 'firebase/app';
-
+import { signInWithEmailAndPassword, getAuth, onAuthStateChanged } from 'firebase/auth';
 
 import Navigation from './src/infrastructure/navigation/AppNavigator';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyABDSrizCTeDDoOGatDfmDx2MhMkrUFWWU',
+  authDomain: 'mealstogo-a8ed8.firebaseapp.com',
+  projectId: 'mealstogo-a8ed8',
+  storageBucket: 'mealstogo-a8ed8.appspot.com',
+  messagingSenderId: '324550778714',
+  appId: '1:324550778714:web:236a50e48e9b6af8ff9d63',
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      signInWithEmailAndPassword(auth, 'sanderson@gmail.com', 'test123456')
+        .then((user) => {
+          setIsAuthenticated(true);
+        })
+        .catch((e) => console.log(e));
+    }, 2000);
+  }, []);
+
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
+
   const [latoLoaded] = useLato({
     Lato_400Regular,
   });
@@ -25,18 +50,7 @@ export default function App() {
     return null;
   }
 
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyABDSrizCTeDDoOGatDfmDx2MhMkrUFWWU",
-    authDomain: "mealstogo-a8ed8.firebaseapp.com",
-    projectId: "mealstogo-a8ed8",
-    storageBucket: "mealstogo-a8ed8.appspot.com",
-    messagingSenderId: "324550778714",
-    appId: "1:324550778714:web:236a50e48e9b6af8ff9d63"
-  };
-
-  initializeApp(firebaseConfig);
-
+  if (!isAuthenticated) return null;
 
   return (
     <>
